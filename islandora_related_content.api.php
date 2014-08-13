9088401cbd1d40b9dffc3bc572950990239f2096
ncphillips <ncphillips.19@gmail.com>
2014-08-13 14:53:48 -0300

    Missed stuff?

--- islandora_related_content.api.php
+++ islandora_related_content.api.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @file Islandora Bioinformatics Hook API
+ * @file Islandora Related Content Hook API
  *
  *       Lists the hooks used by this file.
  */
@@ -62,3 +62,24 @@ function hook_form_islandora_related_RELATED_CMODEL_objects_form_alter(&$form, &
  * and the Subject is of type RELATED_CMODEL
  */
 function hook_form_islandora_OBJECT_CMODEL_related_RELATED_CMODEL_objects_form_alter(&$form, &$form_state){}
+
+/**
+ * @param $data
+ * array(
+ *  'collection' => 'Some Collection PID.',
+ * );
+ */
+function islandora_related_content_ingest_alter(&$data){
+  $data['collection'] = 'islandora:root';
+}
+
+/**
+ * @param $data
+ * array(
+ *  'collection' => 'Some Collection PID.',
+ * );
+ */
+function islandora_related_RELATED_CMODEL_content_ingest_alter(&$data){
+  // If RELATED_CMODEL = islandora_specimen_cmodel
+  $data['collection'] = 'islandora:specimen_collection';
+}
